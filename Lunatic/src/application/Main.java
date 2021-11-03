982398b893feffcf1b5fbd9da2ffc3adc5f2b176
JiWonJang0514 <82877779+JiWonJang0514@users.noreply.github.com>
2021-11-03 11:34:34 +0900

    SettingScene
    설정 화면 씬 추가

--- Lunatic/src/application/Main.java
+++ Lunatic/src/application/Main.java
@@ -12,10 +12,13 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader();
-			loader.setLocation(getClass().getResource("/work/LoginScene.fxml"));
+//			loader.setLocation(getClass().getResource("/work/MainScene.fxml"));
+//			loader.setLocation(getClass().getResource("/work/LoginScene.fxml"));
+			loader.setLocation(getClass().getResource("/work/SettingScene.fxml"));
 			AnchorPane root = (AnchorPane)loader.load();
 			
-			Scene scene = new Scene(root,1200,800);
+//			Scene scene = new Scene(root,1200,800);
+			Scene scene = new Scene(root,350,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setTitle("Lunatic");
 			primaryStage.setScene(scene);
