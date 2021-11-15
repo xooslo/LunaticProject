1decd19e1ac8ce772e60f78ec9a1156f6bb8ba2e
JiWonJang0514 <82877779+JiWonJang0514@users.noreply.github.com>
2021-11-16 01:51:20 +0900

    불러오기/세션

--- SQL 파일/lunatic.sql
+++ SQL 파일/lunatic.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- 생성 시간: 21-11-13 11:51
--- 서버 버전: 10.4.21-MariaDB
--- PHP 버전: 7.4.23
+-- 생성 시간: 21-11-15 17:41
+-- 서버 버전: 10.4.19-MariaDB
+-- PHP 버전: 7.4.20
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,6 +24,24 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- 테이블 구조 `log_info`
+--
+
+CREATE TABLE `log_info` (
+  `id` varchar(30) NOT NULL,
+  `nick` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- 테이블의 덤프 데이터 `log_info`
+--
+
+INSERT INTO `log_info` (`id`, `nick`) VALUES
+('haesun', '1234');
+
+-- --------------------------------------------------------
+
+--
 -- 테이블 구조 `player`
 --
 
@@ -38,10 +56,82 @@ CREATE TABLE `player` (
 --
 
 INSERT INTO `player` (`id`, `nick`, `password`) VALUES
-('', '', ''),
-('', '', ''),
-('', '', ''),
+('fullsun', '??????', '1234'),
+('haesun', 'hae', '1234'),
 ('xooslo', '현재여친', '1234');
+
+-- --------------------------------------------------------
+
+--
+-- 테이블 구조 `save`
+--
+
+CREATE TABLE `save` (
+  `id` int(11) NOT NULL,
+  `stage` int(3) NOT NULL,
+  `potion` varchar(10) NOT NULL,
+  `accessory` varchar(10) NOT NULL,
+  `skin` varchar(10) NOT NULL,
+  `weapon` varchar(10) NOT NULL,
+  `player_id` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- 테이블의 덤프 데이터 `save`
+--
+
+INSERT INTO `save` (`id`, `stage`, `potion`, `accessory`, `skin`, `weapon`, `player_id`) VALUES
+(1, 2, 'p001', 'a001', 's001', 'w001', 'xooslo'),
+(2, 1, 'p001', 'a001', 's001', 'w001', 'haesun');
+
+--
+-- 덤프된 테이블의 인덱스
+--
+
+--
+-- 테이블의 인덱스 `log_info`
+--
+ALTER TABLE `log_info`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 테이블의 인덱스 `player`
+--
+ALTER TABLE `player`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 테이블의 인덱스 `save`
+--
+ALTER TABLE `save`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `player_id` (`player_id`);
+
+--
+-- 덤프된 테이블의 AUTO_INCREMENT
+--
+
+--
+-- 테이블의 AUTO_INCREMENT `save`
+--
+ALTER TABLE `save`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- 덤프된 테이블의 제약사항
+--
+
+--
+-- 테이블의 제약사항 `log_info`
+--
+ALTER TABLE `log_info`
+  ADD CONSTRAINT `session_id` FOREIGN KEY (`id`) REFERENCES `player` (`id`);
+
+--
+-- 테이블의 제약사항 `save`
+--
+ALTER TABLE `save`
+  ADD CONSTRAINT `player_id` FOREIGN KEY (`player_id`) REFERENCES `player` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
